682bc3d16a1e37adeba0014e0bad636ecbb32353
User <user@dhcp-10-8-037-088.mobile.reshsg.uci.edu>
2023-04-05 01:01:21 -0700

    mapty app

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -10,3 +10,43 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+      const map = L.map('map').setView([latitude, longitude], 13);
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      }).addTo(map);
+      L.marker([latitude, longitude])
+        .addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
+
+        map.on('click', function(mapEvent) {
+            console.log(mapEvent);
+            const {lat, lng} = mapEvent.latlng;
+            L.marker([lat, lng])
+            .addTo(map)
+            .bindPopup(
+                L.popup({
+                maxWidth: 250,
+                minWidth: 100,
+                autoClose: false,
+                closeOnClick: false,
+                className: 'running-popup',
+                })
+            )
+            .setPopupContent('Workout')
+            .openPopup();
+        })
+    },
+    function () {
+      alert('Could not get your position');
+    }
+  ); // Geolocation API
